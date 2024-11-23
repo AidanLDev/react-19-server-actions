46ddc54ec3168889046a51222f3a1d3862941df9
Aidan Lowson <dev@aidanlowson.com>
2024-11-23 18:06:08 +0000

    Finishing backend-stack for creating dynamo table

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const notesTable = new dynamodb.Table(this, "NotesTable", {
+      partitionKey: {
+        name: "Id",
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "NotesTableName", {
+      value: notesTable.tableName,
+      description: "Notes DynamoDB table name"
+    })
+
+    new cdk.CfnOutput(this, "NotesTableArn", {
+      value: notesTable.tableArn,
+      description: "Notes DynamoDB ARN"
+    })
+
   }
 }
