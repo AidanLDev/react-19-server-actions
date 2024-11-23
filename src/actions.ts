c98fe8c50dd7238aa08d01d1871ba448198c4cd6
Aidan Lowson <dev@aidanlowson.com>
2024-11-23 19:55:12 +0000

    Sending an API req to AWS

--- src/actions.ts
+++ src/actions.ts
@@ -1,12 +1,29 @@
 'use server';
 
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  PutItemCommandInput,
+} from '@aws-sdk/client-dynamodb';
 import { INoteState } from './types';
+import { v4 as uuidv4 } from 'uuid';
 
 export async function createNoteAction(
-  prevState: INoteState,
+  _: INoteState,
   queryData: FormData
 ): Promise<INoteState> {
-  console.log('prevState: ', prevState);
+  const config = {
+    credentials: {
+      accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY as string,
+    },
+    region: process.env.REACT_APP_AWS_REGION,
+  };
+
+  console.log(config);
+  console.log(process.env.REACT_APP_NOTES_TABLE_NAME)
+
+  const client = new DynamoDBClient(config);
   const newNote = queryData.get('note');
 
   if (!newNote || typeof newNote !== 'string') {
@@ -16,9 +33,26 @@ export async function createNoteAction(
     };
   }
 
+  const putInput: PutItemCommandInput = {
+    TableName: process.env.REACT_APP_NOTES_TABLE_NAME as string,
+    Item: {
+      Id: { S: uuidv4() },
+      Note: { S: newNote },
+    },
+  };
+
   try {
-    await new Promise((resolve) => setTimeout(resolve, 1500));
-    console.log('Have this note in the API!: ', newNote);
+    const putItemCommand = new PutItemCommand(putInput);
+    const putItemResponse = await client.send(putItemCommand);
+
+    console.log(putItemResponse);
+
+    if (putItemResponse.$metadata.httpStatusCode === 200) {
+      console.log('Successfully added it to le db');
+    } else {
+      console.error('Failed to put item');
+      throw new Error('Response was not 200');
+    }
   } catch (err) {
     console.error(`Failed to upsert note to the DB ${err}`);
     return {
